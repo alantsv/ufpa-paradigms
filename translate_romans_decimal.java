3315c663081664c956d89f00081343a235cdba78
Alan Veloso <alantsv@gmail.com>
2017-06-21 15:22:05 -0300

    Update translate_romans-decimal (WIP)

--- translate_romans_decimal.java
+++ translate_romans_decimal.java
@@ -1,4 +1,3 @@
-// ver arquivo em Prolog
 class Primitivas { 
   public static String s; 
   public static void le_fita(String fita){ s = fita; } 
@@ -21,10 +20,10 @@ class Primitivas {
 //*****************************************
 class Romanos extends Primitivas { 
   public static boolean RS( String[] V, int[] i, int[] o ) { 
-  return (dd(V, i, o) 
+  return (ddd(V, i, o) 
+    || dd(V, i, o) 
     || d(V, i, o)); 
   } 
-
   public static boolean d(String[] V, int[] i, int[] o) { 
     return (tok("0", i, o) && attr(V, "") 
         || tok("1", i, o) && attr(V, "I") 
@@ -51,25 +50,47 @@ class Romanos extends Primitivas {
         || tok("8", i, i1) && d(D, i1, o) && attr(V, "LXXX" + D[0]) 
         || tok("9", i, i1) && d(D, i1, o) && attr(V, "XC" + D[0]));
   }
+  public static boolean ddd(String[] V, int[] i, int[] o) { 
+    int[] i1 = {0}; 
+    String[] D = {""}; 
+    return (tok("0", i, i1) && dd(D,i1, o) && attr(V, "" + D[0])
+	      || tok("1", i, i1) && dd(D, i1, o) && attr(V, "C" + D[0]) 
+	      || tok("2", i, i1) && dd(D, i1, o) && attr(V, "CC" + D[0]) 
+	      || tok("3", i, i1) && dd(D, i1, o) && attr(V, "CCC" + D[0]) 
+	      || tok("4", i, i1) && dd(D, i1, o) && attr(V, "CD" + D[0])
+	      || tok("5", i, i1) && dd(D, i1, o) && attr(V, "D" + D[0])
+	      || tok("6", i, i1) && dd(D, i1, o) && attr(V, "DC" + D[0])
+	      || tok("7", i, i1) && dd(D, i1, o) && attr(V, "DCC" + D[0])
+        || tok("8", i, i1) && dd(D, i1, o) && attr(V, "DCCC" + D[0]) 
+        || tok("9", i, i1) && dd(D, i1, o) && attr(V, "CM" + D[0]));
+  }
+  public static void write(){
+      if (RS( V1, i, i1 ) && tok( "@", i1, o1 )) { 
+      System.out.println("Decimal: "+s.substring(0, s.length()-1) +" Romano: "+V1[0]); 
+    } 
+    else { 
+      System.out.println("Erro na posicao: "+(pe[0]+1)); 
+    } 
+  }
 }
 
 public class Main extends Romanos {
   public static void main( String args[] ) { 
     int[] i = {0}; int[] i1 = {0}; int[] o1 = {0}; String[] V1 = {""}; 
-    le_fita("97@"); pe[0] = 0; 
+    le_fita("397@"); pe[0] = 0; 
     if (RS( V1, i, i1 ) && tok( "@", i1, o1 )) { 
       System.out.println("Decimal: "+s.substring(0, s.length()-1) +" Romano: "+V1[0]); 
     } 
     else { 
       System.out.println("Erro na posicao: "+(pe[0]+1)); 
     } 
-    // i[0] = 0; i1[0] = 0; o1[0] = 0; String[] V2 = {""}; 
-    // le_fita("IV@"); pe[0] = 0; 
-    // if ( RS( V2, i, i1 ) && tok( "@", i1, o1 ) ) { 
-    //   System.out.println("Romano: "+s.substring(0, s.length()-1) +" Decimal: "+V2[0]); 
-    // } 
-    // else { 
-    //   System.out.println("Erro na posicao: "+(pe[0]+1)); 
-    // } 
+    i[0] = 0; i1[0] = 0; o1[0] = 0; String[] V2 = {""}; 
+    le_fita("IV@"); pe[0] = 0; 
+    if ( RS( V2, i, i1 ) && tok( "@", i1, o1 ) ) { 
+      System.out.println("Romano: "+s.substring(0, s.length()-1) +" Decimal: "+V2[0]); 
+    } 
+    else { 
+      System.out.println("Erro na posicao: "+(pe[0]+1)); 
+    } 
   } 
 } 
